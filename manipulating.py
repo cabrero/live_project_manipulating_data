c765c16fc3011c8537df290f9a8ffcc7921f73e8
David Cabrero <david.cabrero@madsgroup.org>
2020-10-29 18:18:19 +0100

    2.1. Keeping only data of interest

--- manipulating.py
+++ manipulating.py
@@ -5,6 +5,17 @@ import sys
 import numpy as np
 import pandas as pd
 
+def change_row(row):
+    row['Host city'] = row['Host city'].split(',')[0]
+    try:
+        result = row['Result'].replace(',', ' ').split()
+        east, west = [ int(s) for s in result if s.isdigit() ]
+    except Exception:
+        print(row['Result'])
+        east, west = np.NaN, np.NaN
+    row['East'] = east
+    row['West'] = west
+    return row
 
 tables = pd.read_html("https://en.wikipedia.org/wiki/NBA_All-Star_Game")
 header = pd.Index(['Year', 'Result', 'Host arena', 'Host city', 'Game MVP'])
@@ -16,5 +27,10 @@ for table in tables:
 if df is None:
     print("Couldn't find results table")
     sys.exit(1)
-df = df.dropna()
+
+    
+df.insert(2, "East", 0)
+df.insert(3, "West", 0)
+df = df.apply(change_row, axis= 'columns').drop(columns= ["Result", "Host arena", "Game MVP"]).dropna()
+
 print(df)
